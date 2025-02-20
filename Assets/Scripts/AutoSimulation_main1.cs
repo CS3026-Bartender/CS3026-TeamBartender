32877687eb4ef3fc3ca9d68ba09ba83c5b9e0209
andj230 <andj230@gmail.com>
2025-02-19 17:59:57 -0700

    Created Script
    Created Script that will eventually run the autosimulation.

---
+++ Assets/Scripts/AutoSimulation_main1.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class AutoSimulation_main1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Test");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
