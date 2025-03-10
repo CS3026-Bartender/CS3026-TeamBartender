7d9024c87270d2ea16f364f5cb420f9d2bd7c830
hxhdrn <kobisaha@gmail.com>
2025-03-10 16:24:50 -0600

    Minor change to Spirit

--- Assets/Scripts/Data/Spirit.cs
+++ Assets/Scripts/Data/Spirit.cs
@@ -3,15 +3,14 @@ using UnityEngine;
 
 public class Spirit : Ingredient
 {
-    // constructor = ingredient constructor
-    public Spirit(string name, float price, string desc, Sprite sprite) : base(name, price, desc, sprite) { }
-
-    private Dictionary<string, float> stats = new(); // stat id, stat value
-
-    // get a stat base value from a stat id
-    public float GetStat(string id)
+    public Spirit(string name, float price, string desc, Sprite sprite, float simPrice, float serveTime)
+        : base(name, price, desc, sprite)
     {
-        return stats.GetValueOrDefault(id);
+        SimPrice = simPrice;
+        ServeTime = serveTime;
     }
+
+    public float SimPrice { get; private set; }
+    public float ServeTime { get; private set; }
 }
 
