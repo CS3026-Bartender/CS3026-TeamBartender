128916bd0a20d3e424d35c627dc8ead6ff1c23f3
hxhdrn <kobisaha@gmail.com>
2025-03-12 12:35:22 -0600

    Drink menu display updates slots

--- Assets/Scripts/Shop-Menu/DrinkMenuDisplay.cs
+++ Assets/Scripts/Shop-Menu/DrinkMenuDisplay.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class DrinkMenuDisplay : MonoBehaviour
 {
     public void RefreshDisplay()
     {
         // TODO: go through each drink/slot and update with data from DrinkData
+        for (int i = 0; i < 3; i++)
+        {
+            Drink drink = DrinkData.Instance.GetDrink(i);
+            if (drink == null)
+            {
+
+            }
+        }
     }
 
     public void HighlightSlots(bool[][] data)
--- Assets/Scripts/Shop-Menu/Slot.cs
+++ Assets/Scripts/Shop-Menu/Slot.cs
@@ -6,9 +6,24 @@ public class Slot : MonoBehaviour
 {
     [SerializeField] protected IngredientDisplay ingDisplay;
     [SerializeField] protected DropArea dropArea;
+    [SerializeField] protected GameObject ingDisplayPrefab;
 
     public virtual void UpdateSlot(string newID)
     {
+        if (ingDisplay == null)
+        {
+            GameObject newIngDisplayObj = Instantiate(ingDisplayPrefab, transform.position, transform.rotation, transform);
+            ingDisplay = newIngDisplayObj.GetComponent<IngredientDisplay>();
+        }
         ingDisplay.UpdateDisplay(newID);
+    }
+
+    public virtual void SetEmpty()
+    {
+        if (ingDisplay != null)
+        {
+            Destroy(ingDisplay.gameObject);
+            ingDisplay = null;
+        }
     }
 }
