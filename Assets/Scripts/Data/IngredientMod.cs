7994524ec2af8fd4b198710e4e34be2532750989
Kee-T <116755980+Kee-T@users.noreply.github.com>
2025-04-28 16:10:50 -0600

    Update and rename IngredientMod to IngredientMod.cs

--- Assets/Scripts/Data/IngredientMod.cs
+++ Assets/Scripts/Data/IngredientMod.cs
@@ -5,3 +5,11 @@ public class IngredientMod
   public ModifierType Type { get; }
   public string StatID { get; }
   public float Value { get; }
+
+  public IngredientMod(ModifierType type, string statID, float value)
+  {
+    Type = type;
+    StatID = statID;
+    Value = value;
+  }
+}
