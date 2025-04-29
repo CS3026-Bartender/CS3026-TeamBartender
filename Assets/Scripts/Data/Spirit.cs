4ce0dbad4d46d8968e31c11ea8fec1ea1843b778
Kee-T <116755980+Kee-T@users.noreply.github.com>
2025-04-29 16:41:44 -0600

    Update Spirit.cs

--- Assets/Scripts/Data/Spirit.cs
+++ Assets/Scripts/Data/Spirit.cs
@@ -5,6 +5,8 @@ public class Spirit : Ingredient
     public float BaseServeTime { get; private set; }
     public float BaseCustomerDrinkTime { get; private set; }
     public float BasePotency { get; private set; }
+
+    public Dictionary<string, float> BaseStats { get; private set; }
     // constructor = ingredient constructor
     public Spirit(string name, float price, float sellPrice, string desc, Sprite sprite,
                  float serveTime, float customerDrinkTime, float potency,
