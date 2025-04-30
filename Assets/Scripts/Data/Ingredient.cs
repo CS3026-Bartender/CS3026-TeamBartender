3559c9746207d13135e5dd381a3be4764c21e6af
hxhdrn <kobisaha@gmail.com>
2025-04-30 10:27:35 -0600

    Fixed conflicts

--- Assets/Scripts/Data/Ingredient.cs
+++ Assets/Scripts/Data/Ingredient.cs
@@ -8,14 +8,11 @@ public class Ingredient : DrinkComponent
     public float StatMod { get; set; }
     public bool IsMult { get; set; }
 
-    public List<IngredientMod> Mods { get; private set; } 
-
     public Ingredient(string name, float price, float sellPrice, string desc, Sprite sprite,
                      string statID, float statMod, bool isMult) : base(name, price, sellPrice, desc, sprite)
     {
         StatID = statID;
         StatMod = statMod;
         IsMult = isMult;
-        Mods = new List<IngredientMod>();
     }
 }
